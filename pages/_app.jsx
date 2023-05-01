79b08749eac3ffb8655db17bede3129fed673fb2
vic m <vm@vics-MacBook-Pro.local>
2023-05-01 14:19:11 +0300

    feat: wrapped app in chakra ui provider

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,5 +1,15 @@
-import '@/styles/globals.css'
+import { ChakraProvider } from "@chakra-ui/react";
+
+import "@/styles/globals.css";
+import theme from "@/components/themes/theme";
+import Layout from "@/components/layout";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ChakraProvider>
+  );
 }
