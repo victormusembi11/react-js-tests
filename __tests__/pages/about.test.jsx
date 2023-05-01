95526461bab9a811474514315753038ee74ff10a
vic m <vm@vics-MacBook-Pro.local>
2023-05-01 18:04:25 +0300

    tests: about page button & handle click func

--- __tests__/pages/about.test.jsx
+++ __tests__/pages/about.test.jsx
@@ -12,4 +12,18 @@ describe("About", () => {
     const headerText = "About";
     expect(header).toHaveTextContent(headerText);
   });
+
+  it("renders a button", () => {
+    render(<About />);
+    const button = screen.getByRole("button");
+    expect(button).toBeInTheDocument();
+  });
+
+  it("logs a message when the button is clicked", () => {
+    const spy = jest.spyOn(console, "log");
+    render(<About />);
+    const button = screen.getByRole("button");
+    button.click();
+    expect(spy).toHaveBeenCalledWith("clicked");
+  });
 });
